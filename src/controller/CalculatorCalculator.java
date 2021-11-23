2202a0f2a79be424147bee91a36deb9bc5fa606e
Oshanlakshith <oshanlakshitha146@gmail.com>
2021-11-23 21:23:23 +0530

    'Adding'

--- src/controller/CalculatorCalculator.java
+++ src/controller/CalculatorCalculator.java
@@ -1,5 +1,22 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class CalculatorCalculator {
 
+    public TextField txtone;
+    public TextField txttwo;
+    public Label lblAnswer;
+
+    public void btnAdding(ActionEvent actionEvent) {
+        int number1,number2;
+        int answer=0;
+
+        number1=Integer.parseInt(txtone.getText());
+        number2=Integer.parseInt(txttwo.getText());
+        answer=number1+number2;
+        lblAnswer.setText(answer+"");
+    }
 }
