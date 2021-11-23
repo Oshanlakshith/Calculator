820e47d87b5fe07dfc6c3b4309aa73eaccfc6595
Oshanlakshith <oshanlakshitha146@gmail.com>
2021-11-23 21:33:34 +0530

    'miness,increes,divide'

--- src/controller/CalculatorCalculator.java
+++ src/controller/CalculatorCalculator.java
@@ -10,13 +10,38 @@ public class CalculatorCalculator {
     public TextField txttwo;
     public Label lblAnswer;
 
-    public void btnAdding(ActionEvent actionEvent) {
-        int number1,number2;
-        int answer=0;
+    int number1,number2;
+    int answer=0;
 
+    public void btnAdding(ActionEvent actionEvent) {
         number1=Integer.parseInt(txtone.getText());
         number2=Integer.parseInt(txttwo.getText());
+
         answer=number1+number2;
+        lblAnswer.setText(answer+"");
+    }
+
+    public void btnMiness(ActionEvent actionEvent) {
+        number1=Integer.parseInt(txtone.getText());
+        number2=Integer.parseInt(txttwo.getText());
+
+        answer=number1-number2;
+        lblAnswer.setText(answer+"");
+    }
+
+    public void btnIncrease(ActionEvent actionEvent) {
+        number1=Integer.parseInt(txtone.getText());
+        number2=Integer.parseInt(txttwo.getText());
+
+        answer=number1*number2;
+        lblAnswer.setText(answer+"");
+    }
+
+    public void btmdivide(ActionEvent actionEvent) {
+        number1=Integer.parseInt(txtone.getText());
+        number2=Integer.parseInt(txttwo.getText());
+
+        answer=number1/number2;
         lblAnswer.setText(answer+"");
     }
 }
